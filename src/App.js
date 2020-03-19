0fe7899c9c36f013b1817ac5b71cf9384b2d7c65
fer <fernanda.1991.alejandra@gmail.com>
2020-03-19 12:08:03 -0300

    creacion de here

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import location from './here.js'
 
 function App() {
+ 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          WheelGo!
         </p>
+        <nav.link>
+
+        </nav.link>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
