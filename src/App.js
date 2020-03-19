4cc7509927090c33e72cc8773137fcb3add2a4f2
camila-osores <camilaosoresa@gmail.com>
2020-03-19 08:39:32 -0300

    conectando con firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {useFirebaseApp} from 'reactfire';
 
 function App() {
+  const firebase = useFirebaseApp();
+  console.log(firebase)
   return (
     <div className="App">
       <header className="App-header">
