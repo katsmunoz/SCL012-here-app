522150009d2cdcd0ff722ab8fd2be8899b183288
camila-osores <56646586+camila-osores@users.noreply.github.com>
2020-03-23 11:59:42 -0300

    Firebase (#7)
    * conectando con firebase (#1)
    
    * creacion de here (#4)
    
    * Login (#6)
    
    * conectando con firebase
    
    * inicializando firebase
    
    * creando login, logout y cuenta nueva en componente auth
    
    Co-authored-by: fer221 <56723806+fer221@users.noreply.github.com>
    Co-authored-by: Katherine Muñoz <56646813+katsmunoz@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import {useFirebaseApp} from 'reactfire';
+import Auth from './Auth';
+import {useUser} from 'reactfire';
+
 
 function App() {
-  const firebase = useFirebaseApp();
-  console.log(firebase)
-
+  const user= useUser();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          WheelGo!
-        </p>
-        <nav.link>
+      {user && <p>Bienvenido {user.email}</p>}
+      <Auth />
 
-        </nav.link>
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
