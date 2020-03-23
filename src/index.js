221fa9eff90f18757469980239e104f1dbfeeae8
camila-osores <camilaosoresa@gmail.com>
2020-03-23 10:54:08 -0300

    creando login, logout y cuenta nueva en componente auth

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import firebaseConfig from './firebase-config';
-
 import {FirebaseAppProvider} from 'reactfire';
 
 ReactDOM.render((
