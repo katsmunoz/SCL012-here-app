1463ddbcae1d433d4b5095b8f43cb421c2021316
Katherine Muñoz <56646813+katsmunoz@users.noreply.github.com>
2020-04-01 11:40:12 -0300

    integranto Here maps component (#11)
    * conectando con firebase (#1)
    
    * creacion de here (#4)
    
    * Login (#6)
    
    * conectando con firebase
    
    * inicializando firebase
    
    * creando login, logout y cuenta nueva en componente auth
    
    * creacion de here (#9)
    
    Co-authored-by: camila-osores <56646586+camila-osores@users.noreply.github.com>
    Co-authored-by: fer221 <56723806+fer221@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 import Auth from './Auth';
 import {useUser} from 'reactfire';
 
-
 function App() {
   const user= useUser();
   return (
     <div className="App">
       {user && <p>Bienvenido {user.email}</p>}
       <Auth />
+    
     </div>
   );
 }
